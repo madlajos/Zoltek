f1b4c5360bf060f96d028ee52d3b1efd33efbcea
lillaalexandra <m.lillaalexandra@gmail.com>
2025-04-07 10:00:20 +0200

    Fix: Home init fix

--- PythonBackend/image_processing/homeTurntable/__init__.py
+++ PythonBackend/image_processing/homeTurntable/__init__.py
@@ -8,14 +8,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from .preprocessing import preprocess, rotate_image, crop_second_two_thirds, fill_second_two_thirds
 from .home_tempmatch import start_temp_match
-from .home_fin import find_best_match_and_angle
 # Import from GeneralProc
 from GeneralProc.load_templ import load_template
 
 # Define what gets imported when calling `from homeTurntable import *`
 __all__ = [
     "preprocess", "rotate_image",
-    "crop_second_two_thirds", "fill_second_two_thirds", "start_temp_match"
+    "crop_second_two_thirds", "fill_second_two_thirds", "start_temp_match",
+
 ]
 
 # Set base directory
